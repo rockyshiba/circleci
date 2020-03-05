edd8e6f26cdf6f7f478b5cc4b7f7fd2ea89c7661
rockyshiba <leejsitu@gmail.com>
2020-03-05 23:36:23 +0900

    failed test

--- __tests__/math.test.js
+++ __tests__/math.test.js
@@ -3,5 +3,9 @@ import { rectangleArea } from "../math";
 describe("rectangleArea", () => {
     it("returns the correct area", () => {
         expect(rectangleArea(1, 1)).toBe(1);
-    })
+    });
+
+    it("this will fail", () => {
+        return false;
+    });
 });
